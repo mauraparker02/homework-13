963abd43e93ff3390324effdc83980f660d56e68
mauraparker02 <mauraparker02@gmail.com>
2020-05-02 21:25:47 -0400

    creating database that determines which shakeshack burgers were eatten by id

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) VALUES ('ShackBurger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Shack Stack');
+INSERT INTO burgers (name) VALUES ('SmokeShack');
+INSERT INTO burgers (name) VALUES ('Shroom Burger');
+
+UPDATE burgers SET devour = true WHERE id= 1;
+UPDATE burgers SET devour = true WHERE id= 2;
