20923bcd63a6c5c7fecc2eaec2164ba6b681541a
mauraparker02 <mauraparker02@gmail.com>
2020-05-02 22:10:38 -0400

    setting up where the routes will be eventually

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+var router = express.Router();
+
+// throw in some routes in here eventually once I get the ORM working. 
+
+
+
+module.exports = router;
