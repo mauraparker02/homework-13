acadef4a3f02ae8ab2b1238bf8e1d78799259717
mauraparker02 <mauraparker02@gmail.com>
2020-06-01 20:09:17 -0400

    getting JAWS DB configured in the connection

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,17 @@
 var mysql = require("mysql2");
 
 var connection;
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
   connection = mysql.createConnection({
-    host: "icopoghru9oezxh8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "ol5v2cx6tbhnkj8p",
-    password: "hwom8xrs214f5vus",
-    database: "gw2engge3hpmkwhb"
+    user: "root",
+    password: "passwordisroot",
+    database: "burger_db"
   });
+}
 
 // Throw an error if 
 connection.connect(function(err) {
